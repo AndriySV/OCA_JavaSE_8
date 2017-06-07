cb832ccaf75e56228daa36eeae844de923da69b5
AndriySV <a.s.elips@gmail.com>
2017-06-07 17:33:14 +0300

    test data

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,6 @@ package com;
 public class Main {
 
     public static void main(String... g) {
-
         System.out.println("Hello");
 
     }
---
+++ src/com/NumericPromotion.java
@@ -0,0 +1,28 @@
+package com;
+
+public class NumericPromotion {
+
+	public void sample() {
+		Float f = 100F;
+		Long l = 100L;
+		Double d = 100D;
+
+		/*
+		Float f1 = 100;
+		Long l1 = 100;
+		Double d1 = 100;
+		*/
+
+		float f2 = 100;
+		long l2 = 100;
+		double d2 = 100;
+
+		Byte b = 100;
+		Short sh = 100;
+		Integer i = 100;
+
+
+		float fl = 5;
+		float fl1 = 5.0F;
+	}
+}
